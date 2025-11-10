2fdc9b723e572c95f164986ad9083b48ba7b3e6d
Saurav Negi <saurav.negi1301@gmail.com>
2025-11-11 01:15:14 +0530

    2nd day

---
+++ src/helper.py
@@ -0,0 +1,61 @@
+# from langchain.document_loaders import PyPDFLoader , DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+# from langchain.schema import Document
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def load_pdf_files(data):
+    loader=DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+
+def filter_to_minimal_docs( docs:List[Document]) -> List[Document]:
+    """
+    given a list of documents that are filtered based on sources
+    """
+
+    minmal_docs:List[Document]=[]
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minmal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source":src}
+            )
+        )
+
+    return minmal_docs
+
+
+# splitting the documents into smaller chunks 
+def split_documents(minmal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    text_chunks = text_splitter.split_documents(minmal_docs)
+    return text_chunks
+
+
+def downloade_embeddings():
+    """
+    download the embedding model from the hugging face
+    """
+
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings=HuggingFaceEmbeddings(
+        model_name=model_name,
+    )
+
+    return embeddings
+
+embeddings = downloade_embeddings()
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt =(
+    "before every answer tell the user this is a medical advice bot developed by Saurav Singh Negi."
+    "you are an medical assistance for medical related question answering task."
+    "use the following pieces of retrieved context to answer"
+    "the question. if you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "use three sentences maximum to answer the question."
+    "\n\n"
+    "{context}"
+)
